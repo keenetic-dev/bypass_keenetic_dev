94e63e8cd279e1bd5432f340b784ae2bcc2ae762
Andrew Kolbinov <andruw781@gmail.com>
2026-04-19 05:30:20 +0400

    fix unblock update self-heal

--- unblock_update.sh
+++ unblock_update.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
-ipset flush unblocktor
-ipset flush unblocksh
-ipset flush unblockvmess
-ipset flush unblockvless
-ipset flush unblocktroj
-ipset flush unblockvpn
+
+ensure_set() {
+	ipset create "$1" hash:net -exist >/dev/null 2>&1
+}
+
+flush_set() {
+	ensure_set "$1"
+	ipset flush "$1" >/dev/null 2>&1
+}
+
+[ -x /opt/etc/ndm/fs.d/100-ipset.sh ] && /opt/etc/ndm/fs.d/100-ipset.sh start
+
+flush_set unblocktor
+flush_set unblocksh
+flush_set unblockvmess
+flush_set unblockvless
+flush_set unblocktroj
+flush_set unblockvpn
 
 if ls -d /opt/etc/unblock/vpn-*.txt >/dev/null 2>&1; then
 for vpn_file_names in /opt/etc/unblock/vpn-*; do
 vpn_file_name=$(echo "$vpn_file_names" | awk -F '/' '{print $5}' | sed 's/.txt//')
 # shellcheck disable=SC2116
 unblockvpn=$(echo unblock"$vpn_file_name")
-ipset flush "$unblockvpn"
+flush_set "$unblockvpn"
 done
 fi
+
+[ -x /opt/etc/ndm/netfilter.d/100-redirect.sh ] && table=nat /opt/etc/ndm/netfilter.d/100-redirect.sh
+[ -x /opt/etc/ndm/netfilter.d/100-redirect.sh ] && table=mangle /opt/etc/ndm/netfilter.d/100-redirect.sh
 
 /opt/bin/unblock_dnsmasq.sh
 /opt/etc/init.d/S56dnsmasq restart
